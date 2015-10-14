a1bb02b134135512174f80f069ef661b5bf49786
Josh Gillies <github@joshgilli.es>
2015-10-14 12:27:26 +1100

    Update to node-matrix-assets package, and refactor asset generator

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 var Importer = require('node-matrix-importer')
-var asset = require('node-matrix-virtual-assets')
+var asset = require('node-matrix-assets')
+var getAssetById = asset.getAssetById
 
-var tree = asset('folder', { name: 'matrix-base' }, [
-  asset('folder', { name: 'Config', link: 'type_2' }, [
+var tree = asset('folder', { name: 'matrix-base' },
+  asset('folder', { name: 'Config', link: 'type_2' },
     asset('site', { name: 'Design' }),
     asset('site', { name: 'Media' }),
     asset('site', { name: 'Users' }),
@@ -10,9 +11,23 @@ var tree = asset('folder', { name: 'matrix-base' }, [
     asset('folder', { name: 'Metadata' }),
     asset('folder', { name: 'Components' }),
     asset('folder', { name: 'Layouts' })
-  ]),
-  asset('site', { name: 'Site' }, [
-    asset('page_standard', { name: 'Home', link: { 'notice': [{ 'index': 'Site' }] } }),
-    asset('page_standard', { name: '404', link: { 'notice': [{ 'archive': 'Site' }] } })
-  ])
-])
+  ),
+  asset('site', { id: 'site', name: 'Site' },
+    asset('page_standard', { name: 'Home', link: { index: getAssetById('site') } },
+      asset('bodycopy', { link: 'type_2', dependant: '1', exclusive: '1' },
+        asset('bodycopy_div', { link: 'type_2', dependant: '1' },
+          asset('content_type_wysiwyg', { id: 'test', dependant: '1', exclusive: '1' })
+        )
+      )
+    ),
+    asset('page_standard', { name: '404', link: ['type_2', { archive: getAssetById('site') }] },
+      asset('bodycopy', { link: 'type_2', dependant: '1', exclusive: '1' },
+        asset('bodycopy_div', { link: 'type_2', dependant: '1' },
+          asset('content_type_wysiwyg', { id: 'test', dependant: '1', exclusive: '1' })
+        )
+      )
+    )
+  )
+)
+
+console.log(JSON.stringify(tree, true, 2))
